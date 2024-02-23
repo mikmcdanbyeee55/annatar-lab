33e5077edb8b824cd60639db035f5f62ace829ed
mick-25 <158113781+mick-25@users.noreply.github.com>
2024-02-23 05:36:01 +0000

    Update jackett.py

--- annatar/jackett.py
+++ annatar/jackett.py
@@ -25,8 +25,8 @@ from annatar.torrent import Torrent
 log = structlog.get_logger(__name__)
 
 
-JACKETT_URL: str = os.environ.get("JACKETT_URL", "http://45.154.87.104:9117")
-JACKETT_API_KEY: str = os.environ.get("JACKETT_API_KEY", "6ebybwn3luj2nwyx4063naqjx82hvwuj")
+JACKETT_URL: str = os.environ.get("JACKETT_URL", "http://195.154.231.70:9117")
+JACKETT_API_KEY: str = os.environ.get("JACKETT_API_KEY", "ar2u3emdwocp9cqcbyiugot0t17s65yl")
 
 JACKETT_MAX_RESULTS = int(os.environ.get("JACKETT_MAX_RESULTS", 10))
 JACKETT_TIMEOUT = int(os.environ.get("JACKETT_TIMEOUT", 6))
@@ -35,7 +35,7 @@ JACKETT_CACHE_MINUTES = timedelta(minutes=int(os.environ.get("JACKETT_CACHE_MINU
 
 JACKETT_INDEXERS_LIST: list[str] = os.environ.get(
     "JACKETT_INDEXERS",
-    "backups,bitsearch,btsow,cheggit,idope,leech24,movieworld,rapidzona,scenetime,seedoff,showrss,sktorrent-org,solidtorrents,subsplease,therarbg,torlock,torrentdownloads,torrenting,torrentleech,torrentseeds-api,torrentz2nz,yourbittorrent,limetorrents,badasstorrents",
+    "aither-api,avistaz,bwtorrents,filelist,greatposterwall,morethantv-api,privatehd,rutracker,torrentleech",
 ).split(",")
 
 
